4b1b19167d6dce6d649ccb4e5c769a0c43cbac13
ddouglss <ddouglss1999@gmail.com>
2025-08-28 03:03:00 -0300

    atenticando segurança pela URI

--- src/main/java/br/spring_security/config/WebSecurityConfig.java
+++ src/main/java/br/spring_security/config/WebSecurityConfig.java
@@ -2,13 +2,19 @@ package br.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -16,23 +22,35 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig{
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         var admin = User.withUsername("admin")
-                .password("master123")
-                .roles("ADMIN").build();
+                .password(passwordEncoder.encode("master123"))
+                .roles("ADMIN")
+                .build();
 
         var user = User.withUsername("user")
-                .password("user123")
-                .roles("USERS").build();
+                .password(passwordEncoder.encode("user123"))
+                .roles("USER") // note que usei "USER" para combinar com hasRole
+                .build();
+
         return new InMemoryUserDetailsManager(admin, user);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        // Somente para desenvolvimento; em produção use BCryptPasswordEncoder
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").hasRole("ADMIN")
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/users").hasAnyRole("USER", "ADMIN")
+                .anyRequest()
+                        .authenticated()).formLogin(withDefaults());
+        return http.build();
     }
-
-
 
 }
--- src/main/java/br/spring_security/controller/WelcomeController.java
+++ src/main/java/br/spring_security/controller/WelcomeController.java
@@ -1,19 +1,38 @@
 package br.spring_security.controller;
 
+import br.spring_security.config.WebSecurityConfig;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping
-    public String welcome() {
-        return "Bem vindo ao Spring Security!";
+    private final WebSecurityConfig config;
+
+    public WelcomeController(WebSecurityConfig config) {
+        this.config = config;
     }
 
+    @GetMapping
+    public String welcome() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated()) {
+            return "Acesso não autorizado";
+        }
+
+        return auth.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ADMIN")) ?
+                "Bem vindo ao Spring Security, administrador!" :
+                "Bem vindo ao Spring Security, usuário!";
+    }
+
+
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USERS')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public String users (){
         return "Acesso permitido";
     }
@@ -21,7 +40,7 @@ public class WelcomeController {
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
     public String admin (){
-        return "Acesso permitido";
+        return "Acesso permitido admin";
     }
 
 }
